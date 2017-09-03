f0962250706989cf81724fa72d27b4915642545c
sofiegraham <sofiegraham@hotmail.com>
2017-09-03 15:27:39 -0700

    Testing branches

---
+++ test.js
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+class Wat {
+	constructor(poop) {
+		this.poop = poop;
+	}
+}
+
+module.exports = Wat;
+=======
+Fake thing.
+>>>>>>> parent of aad9725... Function making
+
+This is a test.
+
+
+This is on branchey!
