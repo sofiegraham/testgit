0055eb0c9b9f089c5b748c8e4fab2f6d5d855629
sofiegraham <sofiegraham@hotmail.com>
2017-09-03 14:32:44 -0700

    Revert "Adding things"
    This reverts commit 6c71aa41357292b8092436482472ce43b3e4b652.

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Wat {
 	constructor(poop) {
 		this.poop = poop;
@@ -6,6 +5,3 @@ class Wat {
 }
 
 module.exports = Wat;
-=======
-Fake thing.
->>>>>>> parent of aad9725... Function making
